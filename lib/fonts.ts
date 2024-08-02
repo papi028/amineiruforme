d90045a4bf17d6b594dd4fa9155fa7ecd5bf6edd
Tony Papi <tonypapi028@gmail.com>
2024-08-02 10:50:28 +0000

    Update fonts.ts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,14 +3,14 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'JetBrains Mono';
-    src: url('/fonts/JetBrainsMono-VariableFont_wght.ttf') format('woff2');
+    src: url('/fonts/JetBrainsMono-VariableFont_wght.ttf') format('ttf');
     font-weight: 400;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'Inter';
-    src: url('/fonts/Inter-Regular.woff2') format('woff2');
+    src: url('/fonts/JetBrainsMono-Italic-VariableFont_wght.ttf') format('ttf');
     font-weight: 400;
     font-style: normal;
   }
